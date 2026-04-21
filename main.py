518524eedd7e46b68e29eef589043c64ae30b11f
Jay <77408854+jauntyjocularjay@users.noreply.github.com>
2026-04-21 11:28:58 -0700

    added dev branch

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-import discord
-from discord.ext import commands
+import random as Random
+import discord as Discord
 import logging
-from dotenv import load_dotenv
 import os
+from discord.ext import commands
+from dotenv import load_dotenv
+
+true = True
+false = False
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-intents = discord.Intents.default()
+intents = Discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
+
+def roll(dice: str = 1d6):
+    
 
 @bot.event
 async def on_ready():
