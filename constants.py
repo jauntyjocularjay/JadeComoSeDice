a65c15e20600bccbccf21202453cf914535236bf
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2026-05-11 08:22:28 -0700

    added mutations constants and dictionary

--- constants.py
+++ constants.py
@@ -40,6 +40,78 @@ PILOT = 'Pilot'
 REPAIR = 'Repair'
 TREAT_INJURY = 'Treat Injury'
 
+MUTATIONS = 'Mutations'
+ABILITY_DECAY = 'Ability Decay'
+ACIDIC_SALIVA = 'Acidic Saliva'
+ADRENALINE_JOLT = 'Adrenaline Jolt'
+BLOOD_HUNGER = 'Blood Hunger'
+BRITTLE_BONES = 'Brittle Bones'
+CLAWS = 'Claws'
+COMBAT_FEAR = 'Combat Fear'
+CYBERNETIC_DEPENDENCY = 'Cybernetic Dependency'
+DARKVISION = 'Darkvision'
+ECHOLOCATOR = 'Echolocator'
+ELASTICITY = 'Elasticity'
+ENERGY_ABSORPTION = 'Energy Absorption'
+ENERGY_DIFFUSION = 'Energy Diffusion'
+ENLARGED_FORM = 'Enlarged Form'
+EXOSKELETON = 'Exoskeleton'
+EXTRA_ARMS = 'Extra Arms'
+EXTRA_DIGITS = 'Extra Digits'
+FANGS = 'Fangs'
+FESTERING_SORES = 'Festering Sores'
+FINS = 'Fins'
+FORCE_BARRIER = 'Force Barrier'
+FORKED_TONGUE = 'Forked Tongue'
+FRAILTY = 'Frailty'
+GAZING_EYE = 'Gazing Eye'
+GILLS = 'Gills'
+GREAT_HORNS = 'Great Horns'
+HEAT_COLD_SUSCEPTIBILITY = 'Heat/Cold Susceptibility'
+HORNS = 'Horns'
+HYPERSENSITIVITY = 'Hypersensitivity'
+LEAPER = 'Leaper'
+LETHARGY = 'Lethargy'
+LIGHT_SENSITIVITY = 'Light Sensitivity'
+LOST_ARM = 'Lost Arm'
+MINDSLAVE = 'Mindslave'
+NEUTRAD_DEPENDENCY = 'Neutrad Dependency'
+PHEROMONE_ATTRACTION = 'Pheromone Attraction'
+PHEROMONE_REPULSION = 'Pheromone Repulsion'
+POISONOUS_BLOOD = 'Poisonous Blood'
+PREHENSILE_TAIL = 'Prehensile Tail'
+PRICKLY_PEAR = 'Prickly Pear'
+RADIOACTIVE = 'Radioactive'
+RAPID_AGING = 'Rapid Aging'
+REDUCED_SPEED = 'Reduced Speed'
+SCALY_ARMOR = 'Scaly Armor'
+SCALY_SKIN = 'Scaly Skin'
+SCENT = 'Scent'
+SECOND_WIND = 'Second Wind'
+SKELETAL_REINFORCEMENT = 'Skeletal Reinforcement'
+SMOKESCREEN = 'Smokescreen'
+STINGER = 'Stinger'
+TAIL = 'Tail'
+TELEKINETIC_MIND = 'Telekinetic Mind'
+TELEPATHY = 'Telepathy'
+TENTACLE = 'Tentacle'
+THICK_FUR_COAT = 'Thick Fur Coat'
+THIN_FUR_COAT = 'Thin Fur Coat'
+THIN_SKIN = 'Thin Skin'
+ULTRA_IMMUNE_SYSTEM = 'Ultra Immune System'
+ULTRAVIOLET_ALLERGY = 'Ultraviolet Allergy'
+UNNATURAL_EYES = 'Unnatural Eyes'
+UNNATURAL_HAIR = 'Unnatural Hair'
+UNNATURAL_SKIN = 'Unnatural Skin'
+UNNATURAL_VOICE = 'Unnatural Voice'
+VENOMOUS_BITE = 'Venomous Bite'
+VEXING_VOICE = 'Vexing Voice'
+WALL_CRAWLER = 'Wall Crawler'
+WEAK_IMMUNE_SYSTEM = 'Weak Immune System'
+WEBBED_DIGITS = 'Webbed Digits'
+WINGS = 'Wings'
+X_RAY_VISION = 'X-Ray Vision'
+
 
 D20 = {
     FUTURED20: {
@@ -74,6 +146,79 @@ D20 = {
             PILOT: f'/{FUTURED20}/{SKILLS}.md#pilot-dex',
             REPAIR: f'/{FUTURED20}/{SKILLS}.md#repair-int',
             TREAT_INJURY: f'/{FUTURED20}/{SKILLS}.md#treat-injury-wis'
+        },
+        MUTATIONS: {
+            INDEX: f'/{FUTURED20}/{MUTATIONS}.md',
+            ABILITY_DECAY: f'/{FUTURED20}/{MUTATIONS}.md#ability-decay-drawback',
+            ACIDIC_SALIVA: f'/{FUTURED20}/{MUTATIONS}.md#acidic-saliva-minor',
+            ADRENALINE_JOLT: f'/{FUTURED20}/{MUTATIONS}.md#adrenaline-jolt-minor',
+            BLOOD_HUNGER: f'/{FUTURED20}/{MUTATIONS}.md#blood-hunger-drawback',
+            BRITTLE_BONES: f'/{FUTURED20}/{MUTATIONS}.md#brittle-bones-drawback',
+            CLAWS: f'/{FUTURED20}/{MUTATIONS}.md#claws-minor',
+            COMBAT_FEAR: f'/{FUTURED20}/{MUTATIONS}.md#combat-fear-drawback',
+            CYBERNETIC_DEPENDENCY: f'/{FUTURED20}/{MUTATIONS}.md#cybernetic-dependency-drawback',
+            DARKVISION: f'/{FUTURED20}/{MUTATIONS}.md#darkvision-minor',
+            ECHOLOCATOR: f'/{FUTURED20}/{MUTATIONS}.md#echolocator-major',
+            ELASTICITY: f'/{FUTURED20}/{MUTATIONS}.md#elasticity-major',
+            ENERGY_ABSORPTION: f'/{FUTURED20}/{MUTATIONS}.md#energy-absorption-major',
+            ENERGY_DIFFUSION: f'/{FUTURED20}/{MUTATIONS}.md#energy-diffusion-minor',
+            ENLARGED_FORM: f'/{FUTURED20}/{MUTATIONS}.md#enlarged-form-major',
+            EXOSKELETON: f'/{FUTURED20}/{MUTATIONS}.md#exoskeleton-major',
+            EXTRA_ARMS: f'/{FUTURED20}/{MUTATIONS}.md#extra-arms-major',
+            EXTRA_DIGITS: f'/{FUTURED20}/{MUTATIONS}.md#extra-digits-cosmetic',
+            FANGS: f'/{FUTURED20}/{MUTATIONS}.md#fangs-minor',
+            FESTERING_SORES: f'/{FUTURED20}/{MUTATIONS}.md#festering-sores-drawback',
+            FINS: f'/{FUTURED20}/{MUTATIONS}.md#fins-cosmetic',
+            FORCE_BARRIER: f'/{FUTURED20}/{MUTATIONS}.md#force-barrier-minor',
+            FORKED_TONGUE: f'/{FUTURED20}/{MUTATIONS}.md#forked-tongue-cosmetic',
+            FRAILTY: f'/{FUTURED20}/{MUTATIONS}.md#frailty-drawback',
+            GAZING_EYE: f'/{FUTURED20}/{MUTATIONS}.md#gazing-eye-major',
+            GILLS: f'/{FUTURED20}/{MUTATIONS}.md#gills-minor',
+            GREAT_HORNS: f'/{FUTURED20}/{MUTATIONS}.md#great-horns-minor',
+            HEAT_COLD_SUSCEPTIBILITY: f'/{FUTURED20}/{MUTATIONS}.md#heat-cold-susceptibility-drawback',
+            HORNS: f'/{FUTURED20}/{MUTATIONS}.md#horns-cosmetic',
+            HYPERSENSITIVITY: f'/{FUTURED20}/{MUTATIONS}.md#hypersensitivity-minor',
+            LEAPER: f'/{FUTURED20}/{MUTATIONS}.md#leaper-minor',
+            LETHARGY: f'/{FUTURED20}/{MUTATIONS}.md#lethargy-drawback',
+            LIGHT_SENSITIVITY: f'/{FUTURED20}/{MUTATIONS}.md#light-sensitivity-drawback',
+            LOST_ARM: f'/{FUTURED20}/{MUTATIONS}.md#lost-arm-drawback',
+            MINDSLAVE: f'/{FUTURED20}/{MUTATIONS}.md#mindslave-drawback',
+            NEUTRAD_DEPENDENCY: f'/{FUTURED20}/{MUTATIONS}.md#neutrad-dependency-drawback',
+            PHEROMONE_ATTRACTION: f'/{FUTURED20}/{MUTATIONS}.md#pheromone-attraction-major',
+            PHEROMONE_REPULSION: f'/{FUTURED20}/{MUTATIONS}.md#pheromone-repulsion-drawback',
+            POISONOUS_BLOOD: f'/{FUTURED20}/{MUTATIONS}.md#poisonous-blood-drawback',
+            PREHENSILE_TAIL: f'/{FUTURED20}/{MUTATIONS}.md#prehensile-tail-major',
+            PRICKLY_PEAR: f'/{FUTURED20}/{MUTATIONS}.md#prickly-pear-major',
+            RADIOACTIVE: f'/{FUTURED20}/{MUTATIONS}.md#radioactive-major',
+            RAPID_AGING: f'/{FUTURED20}/{MUTATIONS}.md#rapid-aging-drawback',
+            REDUCED_SPEED: f'/{FUTURED20}/{MUTATIONS}.md#reduced-speed-drawback',
+            SCALY_ARMOR: f'/{FUTURED20}/{MUTATIONS}.md#scaly-armor-minor',
+            SCALY_SKIN: f'/{FUTURED20}/{MUTATIONS}.md#scaly-skin-cosmetic',
+            SCENT: f'/{FUTURED20}/{MUTATIONS}.md#scent-minor',
+            SECOND_WIND: f'/{FUTURED20}/{MUTATIONS}.md#second-wind-minor',
+            SKELETAL_REINFORCEMENT: f'/{FUTURED20}/{MUTATIONS}.md#skeletal-reinforcement-major',
+            SMOKESCREEN: f'/{FUTURED20}/{MUTATIONS}.md#smokescreen-minor',
+            STINGER: f'/{FUTURED20}/{MUTATIONS}.md#stinger-major',
+            TAIL: f'/{FUTURED20}/{MUTATIONS}.md#tail-minor',
+            TELEKINETIC_MIND: f'/{FUTURED20}/{MUTATIONS}.md#telekinetic-mind-major',
+            TELEPATHY: f'/{FUTURED20}/{MUTATIONS}.md#telepathy-major',
+            TENTACLE: f'/{FUTURED20}/{MUTATIONS}.md#tentacle-major',
+            THICK_FUR_COAT: f'/{FUTURED20}/{MUTATIONS}.md#thick-fur-coat-minor',
+            THIN_FUR_COAT: f'/{FUTURED20}/{MUTATIONS}.md#thin-fur-coat-cosmetic',
+            THIN_SKIN: f'/{FUTURED20}/{MUTATIONS}.md#thin-skin-drawback',
+            ULTRA_IMMUNE_SYSTEM: f'/{FUTURED20}/{MUTATIONS}.md#ultra-immune-system-minor',
+            ULTRAVIOLET_ALLERGY: f'/{FUTURED20}/{MUTATIONS}.md#ultraviolet-allergy-drawback',
+            UNNATURAL_EYES: f'/{FUTURED20}/{MUTATIONS}.md#unnatural-eyes-cosmetic',
+            UNNATURAL_HAIR: f'/{FUTURED20}/{MUTATIONS}.md#unnatural-hair-cosmetic',
+            UNNATURAL_SKIN: f'/{FUTURED20}/{MUTATIONS}.md#unnatural-skin-cosmetic',
+            UNNATURAL_VOICE: f'/{FUTURED20}/{MUTATIONS}.md#unnatural-voice-cosmetic',
+            VENOMOUS_BITE: f'/{FUTURED20}/{MUTATIONS}.md#venomous-bite-major',
+            VEXING_VOICE: f'/{FUTURED20}/{MUTATIONS}.md#vexing-voice-major',
+            WALL_CRAWLER: f'/{FUTURED20}/{MUTATIONS}.md#wall-crawler-minor',
+            WEAK_IMMUNE_SYSTEM: f'/{FUTURED20}/{MUTATIONS}.md#weak-immune-system-drawback',
+            WEBBED_DIGITS: f'/{FUTURED20}/{MUTATIONS}.md#webbed-digits-minor',
+            WINGS: f'/{FUTURED20}/{MUTATIONS}.md#wings-major',
+            X_RAY_VISION: f'/{FUTURED20}/{MUTATIONS}.md#x-ray-vision-major'
         }
     }
 }
